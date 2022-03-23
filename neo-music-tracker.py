8d9451de7032379ea574a246120e5e992ff71f19
guigur <guigur13@gmail.com>
2022-03-23 17:01:46 +0100

    added date to the printout

--- neo-music-tracker.py
+++ neo-music-tracker.py
@@ -17,14 +17,14 @@ currentTitreNom = ""
 currentArtisteNom = ""
 
 while(True):
+    timestamp = datetime.now()
     r = requests.get('http://www.radioneo.org/liveJSON.json')
     data = r.json()
     if(data["titreNom"] != currentTitreNom and data["artisteNom"] != currentArtisteNom):
         currentTitreNom = data["titreNom"]
         currentArtisteNom = data["artisteNom"]
-        print("Adding to the database : '" + currentTitreNom + "' par : '" + currentArtisteNom + "'")
+        print(str(timestamp) + " => Adding to the database : '" + currentTitreNom + "' par : '" + currentArtisteNom + "'")
         conn = sqlite3.connect(dbname)
-        timestamp = datetime.now()
         conn.execute("insert into musique (titreNom,artisteNom,heureDiffusion) VALUES (?, ?, ?)", (currentTitreNom, currentArtisteNom, str(timestamp)))
         conn.commit()
         conn.close()
