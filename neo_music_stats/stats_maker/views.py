6f19ec9bec940f8231e205f1240b2e13178a02b7
guigur <guigur13@gmail.com>
2022-03-25 08:16:53 +0100

    working state

--- neo_music_stats/stats_maker/views.py
+++ neo_music_stats/stats_maker/views.py
@@ -2,29 +2,17 @@ from django.shortcuts import render
 import requests
 import sqlite3
 
-def getIdFromSpotify(searchStr):
-    security = {'Authorization': 'Bearer ' + 'BQDsUrPIrtF6I94oCgGyhNv-71p4u7gbYE_RFs-9bQUj_JuFWUjKWYkDfupSFcA0oPdUzFa4sgwJaWSS8giNqP_A11sF7GGhrSECe35NiAJpZaKpH6O_DdN70hGZIDLuzPrV1SQD-Q' }
-    r = requests.get('https://api.spotify.com/v1/search', params={'q': searchStr, 'type': 'track', 'market': 'FR', 'limit': '1'}, headers=security)
-    try:
-        spotifydata = r.json()['tracks']['items'][0]['id']
-    except:
-        print("Exception thrown. x does not exist.")
-        spotifydata = ""
-    print(r.status_code)
-    return (spotifydata)
-
-
 def stats_maker(request):
     dbname = "neo-diff.sqlite"
     conn = sqlite3.connect(dbname)
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM musique LIMIT 20 ORDER BY palyed ASC")
+    cur.execute("SELECT * FROM musique ORDER BY heureDiffusion DESC LIMIT 20 ")
 
     rows = cur.fetchall()
     tracks = []
 
     for row in rows:
-        tracks.append({'spotify': row[3], 'trackName': row[0], 'artistName': row[1], 'played': row[2]})
+        tracks.append({'spotify': row[3], 'trackName': row[0], 'artistName': row[1], 'heureDiffusion': row[2]})
     
     return render(request, 'stats_maker.html', {'tracks': tracks})
--- search.py
+++ search.py
@@ -2,6 +2,7 @@ from pyrsistent import v
 import requests
 from pprint import pprint
 import sqlite3
+import neo_spotify_id_agent
 
 dbname = "neo-diff.sqlite"
 conn = sqlite3.connect(dbname)
@@ -11,18 +12,5 @@ cur.execute("SELECT * FROM musique")
 
 rows = cur.fetchall()
 
-
-def getIdFromSpotify(searchStr):
-    security = {'Authorization': 'Bearer ' + 'BQDsUrPIrtF6I94oCgGyhNv-71p4u7gbYE_RFs-9bQUj_JuFWUjKWYkDfupSFcA0oPdUzFa4sgwJaWSS8giNqP_A11sF7GGhrSECe35NiAJpZaKpH6O_DdN70hGZIDLuzPrV1SQD-Q' }
-    r = requests.get('https://api.spotify.com/v1/search', params={'q': searchStr, 'type': 'track', 'market': 'FR', 'limit': '1'}, headers=security)
-    try:
-        spotifydata = r.json()['tracks']['items'][0]['id']
-    except:
-        print("Exception thrown. x does not exist.")
-        spotifydata = ""
-    print(r.status_code)
-    return (spotifydata)
-
-
 for row in rows:
-    print(getIdFromSpotify(row[0] + ' ' + row[1]))
+    print(neo_spotify_id_agent.getIdFromSpotify(row[0] + ' ' + row[1]))
