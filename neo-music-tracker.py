fa9b7501d0774b15073f00c4cdc9426a846e27f4
Guillaume Arthaud <guigur13@gmail.com>
2022-05-24 12:05:00 +0200

    Update neo-music-tracker.py

--- neo-music-tracker.py
+++ neo-music-tracker.py
@@ -18,19 +18,19 @@ currentArtisteNom = ""
 
 while(True):
     timestamp = datetime.now()
-    r = requests.get('http://www.radioneo.org/liveJSON.json')
-    data = r.json()
-    if(data["titreNom"] != currentTitreNom and data["artisteNom"] != currentArtisteNom):
-        currentTitreNom = data["titreNom"]
-        currentArtisteNom = data["artisteNom"]
-        print(str(timestamp) + " => Adding to the database : '" + currentTitreNom + "' par : '" + currentArtisteNom + "'")
-        conn = sqlite3.connect(dbname)
-        conn.execute("insert into musique (titreNom,artisteNom,heureDiffusion) VALUES (?, ?, ?)", (currentTitreNom, currentArtisteNom, str(timestamp)))
-        conn.commit()
-        conn.close()
-    else:
-        print("Same song, waiting")
-
+    try:
+        r = requests.get('http://www.radioneo.org/liveJSON.json')
+        data = r.json()
+        if(data["titreNom"] != currentTitreNom and data["artisteNom"] != currentArtisteNom):
+            currentTitreNom = data["titreNom"]
+            currentArtisteNom = data["artisteNom"]
+            print(str(timestamp) + " => Adding to the database : '" + currentTitreNom + "' par : '" + currentArtisteNom + "'")
+            conn = sqlite3.connect(dbname)
+            conn.execute("insert into musique (titreNom,artisteNom,heureDiffusion) VALUES (?, ?, ?)", (currentTitreNom, currentArtisteNom, str(timestamp)))
+            conn.commit()
+            conn.close()
+        else:
+            print("Same song, waiting")
+    except:
+        print("Error while getting the good stuff")
     time.sleep(30)
-
-
